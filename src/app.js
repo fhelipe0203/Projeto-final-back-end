5661af9c17060c3610d952ca264baf60dcfee467
fhelipe0203 <octaviouriel2002@gmail.com>
2022-10-06 17:52:37 -0300

    atualizando

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cadastro = require("./routes/users.routes")
 const db = require("./data/mongo")
+const cors = require('cors');
+
 
 
 const app = express();
 app.use(express.json());
-
+app.use(cors({origin: '*'}))
 app.use("/cadastro", cadastro)
 
 module.exports = app
