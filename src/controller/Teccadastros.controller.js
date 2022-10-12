6e63251fc5a6fbba5d8c9510a718d3df8d67a5e4
fhelipe0203 <octaviouriel2002@gmail.com>
2022-10-12 03:04:39 -0300

    corrigindo error da C a c

--- src/controller/Teccadastros.controller.js
+++ src/controller/Teccadastros.controller.js
@@ -5,8 +5,8 @@ const Cadastro = require("../models/tecnicos")
 
 const resultadoCadastro= async (req, res) => {
     try {
-        const Cadastro = await Cadastro.find()
-        return res.status(200).json(Cadastro)
+        const cadastro = await Cadastro.find()
+        return res.status(200).json(cadastro)
     }catch (err){
         res.status(500).json({ message: err.message })
     }
